61286e58b49d297aa25eb16f746e3b999a8c45cd
nanatotibadze <nana.totibadze@gmail.com>
2023-02-03 11:23:36 +0400

    modified js

--- index.js
+++ index.js
@@ -4,17 +4,26 @@ const btn = document.querySelector(".btn");
 const title = document.querySelector(".content__title");
 const text = document.querySelector(".content__text");
 
-let advice = {};
+
+
+const loadAdvice = function (data) {
+
+
+  title.textContent = `Advice #${data.id}`;
+  text.textContent = ` ${data.advice}`;
+};
+
 
 const showAdvice = async function () {
   try {
     const res = await fetch(`https://api.adviceslip.com/advice`);
     const data = await res.json();
 
+
     let { slip } = data;
 
-    advice.id = slip.id;
-    advice.text = slip.advice;
+    loadAdvice(slip);
+
     if (!res.ok) throw new Error("Can't load an Advice, try again later");
   } catch (err) {
     throw err;
@@ -23,11 +32,7 @@ const showAdvice = async function () {
 
 showAdvice();
 
-const loadAdvice = function (data) {
-  showAdvice();
 
-  title.textContent = `Advice #${data.id}`;
-  text.textContent = ` ${data.text}`;
-};
 
-btn.addEventListener("click", () => loadAdvice(advice));
+btn.addEventListener("click", () => showAdvice());
+
